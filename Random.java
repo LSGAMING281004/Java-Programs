4ae43a33acc19b6fa51731ab921ce85ac90224c8
K.LOKESH <135082681+LSGAMING281004@users.noreply.github.com>
2023-08-09 19:40:20 +0530

    Add files via upload

---
+++ Random.java
@@ -0,0 +1,11 @@
+
+import java.util.Random;
+
+class Random{
+    public static void main(String args[])
+    {
+        Random random= new Random();
+        int x = random.nextInt(6);
+        System.out.println(x);
+    }
+}
