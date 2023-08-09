85141711ae23dd52e90e33bd8cf15b1f9e0fd248
K.LOKESH <135082681+LSGAMING281004@users.noreply.github.com>
2023-08-09 20:12:54 +0530

    Add files via upload

---
+++ Delay.java
@@ -0,0 +1,18 @@
+class Delay
+{
+	public static void main(String[] args)
+	{
+		int i,s;
+		for(i= 0 ; i<100 ;i++)
+		{
+			
+			
+			System.out.println("L\3VE YOU");
+			try{
+				Thread.sleep(50);
+			}
+			catch(InterruptedException ls){
+			}
+		}
+	}
+}
