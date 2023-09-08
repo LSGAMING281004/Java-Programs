1e344a135ca0a8d1908393adc6183603b5a42358
K.LOKESH <135082681+LSGAMING281004@users.noreply.github.com>
2023-09-08 22:40:29 +0530

    applet program
    simple emoji

---
+++ face.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.applet.*;
+/*<applet code="face" width=300 hight=200>
+<applet>*/
+public class face extends Applet
+{
+    public void paint(Graphics g)
+    {
+        g.drawLine(30, 40,100,100);
+    }
+}
